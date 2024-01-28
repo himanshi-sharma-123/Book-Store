49136cedc96590dc4be734a0012c541194c5857c
Himanshi Sharma <himanshish456@gmail.com>
2024-01-28 21:15:38 +0530

    about and blog page

--- book-store-frontend-main/src/components/About.jsx
+++ book-store-frontend-main/src/components/About.jsx
@@ -2,8 +2,43 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>about</h1>
+    <div className="px-4 lg:px-24 bg-cyan-200 flex items-center">
+      <div className="flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40">
+        <h1 className="text-5xl font-bold leading-snug text-black">
+          About <span className="text-blue-700">Book Store</span>
+        </h1>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          The E-Book Management System is an application tailored for bookstores
+          of small to medium sizes. This system streamlines bookstore operations
+          by enabling sellers to add new books, images and reference materials
+          using a computerized interface. Incorporating modules for book
+          inventory, this system facilitates tracking of available books and
+          customer profiles. By utilizing this computerized setup, it mitigates
+          the risk of losing book or customer records, a common occurrence in
+          non-computerized systems. These integrated modules empower booksellers
+          to efficiently manage their bookstore, ensuring a more convenient and
+          effective approach compared to traditional, non-computerized bookstore
+          systems. Book store management is designed to deal with the customer
+          queries regarding information about different types of books. This is
+          an online system that matches the customer queries regarding the
+          author, publishers, title, price, latest updates, storage information,
+          upgrades etc. against the information stored in the centralized
+          database. The integration of a chatbot on the website significantly
+          enhances customer satisfaction by swiftly offering comprehensive book
+          details, including categories and genres, through a single chat
+          interaction. The customers of today are not only attracted because
+          online shopping is very convenient, but also because they have broader
+          selections, highly competitive prices, better information about the
+          product. Moreover, business owners often offer online shopping options
+          at low rates because the overhead expenses in opening and running a
+          physical store are higher. Further, with online shopping, their
+          products have access to a worldwide market, which increases the number
+          of customers from different ethnic groups, adds customer value, and
+          overall sustainable in the marketing. To develop an easy to use web
+          based interface where users can search for products, view complete
+          description of the products.
+        </p>
+      </div>
     </div>
   );
 };
--- book-store-frontend-main/src/components/Blog.jsx
+++ book-store-frontend-main/src/components/Blog.jsx
@@ -1,9 +1,91 @@
+import { Card } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>blog</h1>
+    <div className="mt-28 px-4 lg:px-24">
+      <h1 className="text-5xl font-bold text-center">Latest Blog Posts</h1>
+      <div className="grid gap-8 my-12 lg:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 grid-cols-1">
+        <Card className="w-full">
+          <img src="https://www.bookgeeks.in/wp-content/uploads/2024/01/Express-Your-Book-Love-A-Simple-Guide-on-Writing-an-Essay-About-Your-Favorite-Reads.jpg" />
+          <Link href="https://www.bookgeeks.in/express-your-book-love-simple-guide-to-writing-essays-about-favourite-reads/">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-decoration-line: underline">
+              Express Your Book Love: A Simple Guide on Writing an Essay About
+              Your Favorite Reads
+            </h5>
+          </Link>
+          <h6 className="font-bold">
+            by Guest <span>January 28, 2024</span>
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Discover the alchemy of words as you embark on a journey through
+            ‘Express Your Book Love.’ This guide unveils the art of crafting
+            heartfelt essays, inviting you to weave prose that mirrors the depth
+            of your passion for cherished reads.
+          </p>
+        </Card>
+        <Card className="w-full">
+          <img src="https://www.bookgeeks.in/wp-content/uploads/2024/01/Shri-Ram-Janmabhoomi.jpg" />
+          <Link href="https://www.bookgeeks.in/must-read-books-on-the-shri-ram-janmabhoomi-movement/">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-decoration-line: underline">
+              Must-Read Books on Shri Ram Janmabhoomi Movement: Chapters of
+              Courage & Legacy
+            </h5>
+          </Link>
+          <h6 className="font-bold">
+            by Sankalpita Singh <span>January 24, 2024</span>
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Embark on a profound journey through the sacred history of Ayodhya
+            and the Shri Ram Janmabhoomi Movement. Explore the enduring legacy,
+            archaeological revelations, and the unyielding spirit that shaped
+            this monumental movement. Discover the cultural hymn echoing through
+            generations, celebrated in books that unravel the movement’s
+            intellectual depths.
+          </p>
+        </Card>
+        <Card className="w-full">
+          <img src="https://www.bookgeeks.in/wp-content/uploads/2024/01/Iffran-Khan-Books.jpg" />
+          <Link href="https://www.bookgeeks.in/irrfan-khan-books-beyond-acting/">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-decoration-line: underline">
+              Irrfan Khan Beyond Acting: A Literary Journey Through Essential
+              Books and Influences
+            </h5>
+          </Link>
+          <h6 className="font-bold">
+            by Sankalpita Singh <span>January 18, 2024</span>
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Illuminating the global stage, Irrfan Khan’s versatile legacy
+            transcends borders. Explore his journey from acclaimed dramas to
+            Hollywood blockbusters. In our homage, discover four must-read
+            books, delving into his life, art, and friendship with directors.
+            Share your favorite performances and recommendations in the
+            comments. Join us in celebrating this cinematic maestro.{" "}
+          </p>
+        </Card>
+        <Card className="w-full">
+          <img src="https://www.bookgeeks.in/wp-content/uploads/2023/10/Best-Indian-Fantasy-Novels.jpg" />
+          <Link href="https://www.bookgeeks.in/indian-fantasy-novels-unveiling-the-creme-de-la-creme-of-2023/">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-decoration-line: underline">
+              Best Indian Fantasy Novels: Unveiling the Crème de la crème of
+              2023
+            </h5>
+          </Link>
+          <h6 className="font-bold">
+            by Sankalpita Singh <span>October 29, 2023</span>
+          </h6>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            In this article, we have curated a selection of four extraordinary
+            Indian fantasy novels, all freshly published in 2023. These literary
+            gems represent the pinnacle of Indian fantasy writing for this year,
+            showcasing the evolution and expansion of the genre in India. These
+            books are a testament to the boundless creativity of Indian authors,
+            offering readers a portal to captivating new worlds.{" "}
+          </p>
+        </Card>
+      </div>
     </div>
   );
 };
